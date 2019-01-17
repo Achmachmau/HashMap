7bdd4575413a8d2fd85ec8878fc99cbbcceb01e6
Anna <ochinsk@yandex.ru>
2019-01-17 06:03:52 -0800

    Update HashMap.h

--- HashMap.h
+++ HashMap.h
@@ -10,6 +10,22 @@ public:
 	}
 };
 
+/*template<>
+class MyHash<TestClass>
+{
+public:
+	size_t operator()( const TestClass &s ) const
+	{
+		size_t h1 = std::hash<int>()( s.x );
+		return h1;
+		//size_t h2 = std::hash<bool>()( s.tBool );
+		//size_t h3 = std::hash<float>()( s.tFloat );
+		//size_t h4 = std::hash<long long>()( s.tLL );
+		//size_t h5 = std::hash<std::string>()( s.tStr );
+		//return h1 ^ ( h2 << 1 );
+	}
+};*/
+
 template <typename TKey, typename TValue>
 class THashEntry
 {
@@ -21,6 +37,8 @@ public:
 	TKey 	Key;
 	TValue 	Value;
 	size_t 	HashValue;
+
+	THashEntry() {};
 
 	THashEntry( const TKey& key, const TValue& value )
 		: Key(key),
@@ -35,16 +53,17 @@ public:
 		  Value( other.Value ),
 		  HashValue( other.HashValue ) {};
 
-	THashEntry( HashEntry&& tmp )
+	THashEntry( const HashEntry&& tmp )
+		: THashEntry()
 	{
-		Swap( std::move(tmp) );
+		Swap( tmp );
 	}
 
 	~THashEntry()
 	{
 		UsedFlag = false;
-		Key.~TKey();
-		Value.~TValue();
+		//Key.~TKey();
+		//Value.~TValue();
 		HashValue = 0;
 	}
 
@@ -54,17 +73,23 @@ public:
 		Key = other.Key;
 		Value = other.Value;
 		HashValue = other.HashValue;
+		return *this;
 	}
-
+	
 	HashEntry& operator=( HashEntry&& tmp )
 	{
-		Swap( std::move(tmp) );
+		/*UsedFlag = tmp.UsedFlag;
+		Key = static_cast< TKey&& >(tmp.Key);
+		Value = static_cast< TValue&& >( tmp.Value );
+		HashValue = tmp.HashValue;*/
+		Swap( tmp );
 		return *this;
 	}
 
 private:
-	void Swap( HashEntry&& tmp )
+	void Swap( HashEntry& tmp )
 	{
+		std::swap( UsedFlag, tmp.UsedFlag );
 		std::swap( Key, tmp.Key );
 		std::swap( Value, tmp.Value );
 		std::swap( HashValue, tmp.HashValue );
@@ -78,7 +103,7 @@ class THashMap
 	typedef THashMap<TKey, TValue> HashMap;
 
 	HashEntry*	pData;		
-	unsigned	logN = 6;		//!? byte
+	unsigned	logN = 7;		//!? byte
 
 public:
 	THashMap() : pData( nullptr ) {};
@@ -88,18 +113,65 @@ public:
 		Copy( other );
 	}
 
-	THashMap( HashMap&& tmp )
+	THashMap( const HashMap&& tmp )
+		: THashMap()
 	{
 		Swap( std::move( tmp ) );
 	}
 
+	~THashMap()
+	{
+		Clear();
+	}
+
 	void Insert( const TKey& key, const TValue& value )
 	{
+		HashEntry entry( key, value ) ;
+
 		if( !pData )
 		{
 			Allocate();
 		}
-		InsertEntry( HashEntry( key, value ) );
+
+		size_t index = GetHashMod( entry.HashValue ),
+			priceEntry = 0,
+			priceAnotherEntry = 0;
+
+		auto maxShift = GetMaxShift();
+		auto size = BucketCount();
+
+		while( pData[ index ].UsedFlag )
+		{
+			if( entry.Key == pData[ index ].Key )
+				return;
+
+			priceAnotherEntry = GetPrice( index, pData[ index ].HashValue );
+
+			if( priceEntry > priceAnotherEntry )
+			{
+//!!!AAAAAAAAAaaa MoveSemantic onelove!
+				HashEntry tmp( pData[ index ] );
+				pData[ index ] = entry;
+				entry = tmp;
+				//std::swap( entry, pData[ index ] );
+			}
+
+			++index %= size;
+			priceEntry = GetPrice( index, entry.HashValue );
+
+			if( priceEntry > maxShift )
+			{
+				Allocate( logN + 1 );
+
+				index = GetHashMod( entry.HashValue );
+				priceEntry = GetPrice( index, entry.HashValue );
+				maxShift = GetMaxShift();
+				size = BucketCount();
+			}
+		}
+
+		new ( &pData[ index ] ) HashEntry( /*static_cast<HashEntry&&>*/(entry) );
+		return;
 	}
 
 	void Erase( const TKey& key )
@@ -115,10 +187,90 @@ public:
 		return searchResult ? &searchResult->Value : nullptr;
 	}
 
-	size_t Size( unsigned n = 0 )
+	bool Empty()
 	{
-		return n ? ( 1 << n ) + n
-			: ( 1 << logN ) + logN;
+		if( !pData )
+			return true;
+
+		size_t size = BucketCount();
+		for( size_t i = 0; i < size; ++i )
+		{
+			if( pData[ i ].UsedFlag )
+				return false;
+		}
+		return true;
+	}
+
+	size_t Size()
+	{
+		size_t buff = BucketCount(),
+			size = 0;
+		for( size_t i = 0; i < buff; ++i )
+		{
+			if( pData[ i ].UsedFlag )
+			{
+				++size;
+			}
+		}
+		return size;
+	}
+
+	size_t BucketCount( unsigned n = 0 )
+	{
+		return n ? ( 1 << n ) + ( GetMaxShift( n ) >> 1 )
+			: ( 1 << logN ) + ( GetMaxShift() >> 1 );
+	}
+
+	void Clear( bool bBuffClear = true )
+	{
+		size_t size = BucketCount();
+		for( size_t i = 0; i < size; ++i )
+		{
+			if( pData[ i ].UsedFlag )
+			{
+				//pData[ i ].~THashEntry();
+				( ( HashEntry* ) &pData[ i ] )->~THashEntry();
+			}
+		}
+		if( bBuffClear )
+		{
+			std::free( pData );
+			pData = nullptr;
+			logN = 0;
+		}
+	}
+
+	void Rehash( size_t newSize )
+	{
+		if( newSize < BucketCount() )
+			return;
+
+		unsigned newLog = 0;
+		while( newSize > 0 )
+		{
+			++newLog;
+			newSize >>= 1;
+		}
+
+		if( newLog <= logN )
+			return;
+		
+		Allocate( newLog );
+	}
+
+	void Print()
+	{
+		size_t size = BucketCount();
+		for( size_t i = 0; i < size; ++i )
+		{
+			if( pData[ i ].UsedFlag )
+			{
+				std::cout << "[ " << i << " ]\t";
+				std::cout << pData[ i ].Key << "\t";
+				std::cout << std::hex << pData[ i ].HashValue << std::dec;
+				std::cout << std::endl;
+			}
+		}
 	}
 
 private:
@@ -126,8 +278,10 @@ private:
 	{
 		size_t	hashMod = GetHashMod( MyHash<TKey>()( key ) ),
 			index = hashMod;
+		auto maxShift = GetMaxShift();
+		auto size = BucketCount();
 
-		while( index < hashMod + logN )
+		while( index < hashMod + maxShift )
 		{
 			if( !pData[ index ].UsedFlag )
 				return nullptr;
@@ -135,65 +289,36 @@ private:
 			if( pData[ index ].Key == key )
 				return &( pData[ index ] );
 
-			++index;
+			++index %= size;
 		}
 
 		return nullptr;
 	}
 
-	void InsertEntry( HashEntry&& entry )
-	{
-		size_t	hashMod = GetHashMod( entry.HashValue ),
-				index = hashMod;
-
-		while( pData[ index ].UsedFlag ) //unused/deleted
-		{
-			if( entry.Key == pData[ index ].Key )
-				return;
-
-			auto priceAnotherEntry = index - ( pData[ index ].HashValue >> ( 64 - logN ) );
-			auto priceEntry = index - hashMod;
-			if( priceEntry > logN )
-			{
-				Expand();
-				InsertEntry( std::move(entry) );
-				return;
-			}
-
-			if( priceEntry > priceAnotherEntry )
-			{
-				entry = std::move( pData[ index ] );
-			}
-
-			++index;
-		}
-		new ( &pData[ index ] ) HashEntry( entry );
-	}
-
-	void Expand()
-	{
-		++logN;
-		Allocate();
-	}
-
-	void Allocate()
+	void Allocate(unsigned newLog = 5)
 	{
 		if( !pData )
 		{
-			pData = ( HashEntry* ) std::calloc( Size(), sizeof( HashEntry ) );
+			logN = newLog;
+			pData = ( HashEntry* ) std::calloc( BucketCount(), sizeof( HashEntry ) );
 			assert( pData );
 			return;
 		}
 
-		HashEntry* newData = ( HashEntry* ) std::calloc( Size(), sizeof( HashEntry ) );
+		size_t newSize = BucketCount( newLog );
+		HashEntry* newData = ( HashEntry* ) std::calloc( newSize, sizeof( HashEntry ) );
 		assert( newData );
 
-		size_t oldSize = Size(logN-1);
-		for( size_t i = 0; i < oldSize; ++i )
+		size_t size = BucketCount();
+		size_t newIndex = 0;
+
+		logN = newLog;
+
+		for( size_t i = 0; i < size; ++i )
 		{
 			if( pData[ i ].UsedFlag )
 			{
-				size_t newIndex = GetHashMod( pData[ i ].HashValue );
+				newIndex = GetHashMod( pData[ i ].HashValue );
 				while( newData[ newIndex ].UsedFlag )
 				{
 					++newIndex;
@@ -201,7 +326,6 @@ private:
 				std::memcpy( ( HashEntry* ) &newData[ newIndex ], ( HashEntry* ) &pData[ i ], sizeof( HashEntry ) );
 			}
 		}
-
 		std::free( pData );
 		pData = newData;
 	}
@@ -210,9 +334,12 @@ private:
 	{
 		if( !other.pData )
 			return;
+
 		logN = other.logN;
-		Allocate();
-		for( size_t i = 0; i < Size(); ++i )
+		Allocate(logN);
+
+		size_t size = BucketCount();
+		for( size_t i = 0; i < size; ++i )
 		{
 			if( other.pData[ i ].UsedFlag )
 			{
@@ -229,6 +356,20 @@ private:
 
 	size_t GetHashMod( const size_t& hashVal )
 	{
-		return hashVal >> ( 64 - logN );
+		return hashVal >> ( 64 - logN );	//shift right on nbit(size_t)-nbit(NSlot) - up' bits
+	}
+
+//!?
+	unsigned GetMaxShift(unsigned n = 0)
+	{
+		return n ? 2 * n
+			: 2 * logN;
+	}
+
+//!?unsigned
+	size_t GetPrice( const size_t& index, const size_t& hashValue )
+	{
+		auto size = BucketCount();
+		return ( index - GetHashMod( hashValue ) + size ) % size;
 	}
 };
